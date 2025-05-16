4ec4733b2d4417aaaf7c031b5fcda5a9ad204924
degbessou <dedokema@gmail.com>
2025-05-15 22:58:31 -0400

    Finalisation du footer avec ajout des différents icons et liens

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -3,59 +3,41 @@ import "../styles/font.css";
 
 const Footer = () => {
     return (
-        <footer className="footer bg-base-200 text-base-content border-base-300 border-t px-10 py-4">
+        <footer className="footer bg-base-300 text-base-content border-base-300 border-t px-24 py-4">
             <aside className="grid-flow-col items-center">
-                <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    className="fill-current">
-                    <path
-                        d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
+                    <defs>
+                        <clipPath id="squareClip">
+                            <rect x="0" y="0" width="40" height="40" rx="20" />
+                        </clipPath>
+                    </defs>
+                    <g clipPath="url(#squareClip)">
+                        <rect x="0" y="0" width="72" height="72" fill="#000000" />
+                        <g transform="translate(20 20)">
+                            <g transform="scale(0.08)">
+                                <g transform="translate(-128 -128)">
+                                    <path d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40ZM104,176a8,8,0,0,1,0,16H72a8,8,0,0,1-8-8V72a8,8,0,0,1,8-8h32a8,8,0,0,1,0,16H80v96Zm88,8a8,8,0,0,1-8,8H152a8,8,0,0,1,0-16h24V80H152a8,8,0,0,1,0-16h32a8,8,0,0,1,8,8Z" fill="#FFFFFF"></path>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
                 </svg>
                 <p>
-                    ACME Industries Ltd.
+                    Portfolio réalisé avec passion par <strong>DEGBESSOU DONALD</strong>
                     <br />
-                    Providing reliable tech since 1992
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
                 </p>
             </aside>
             <nav className="md:place-self-center md:justify-self-end">
                 <div className="grid grid-flow-col gap-4">
-                    <a>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            className="fill-current">
-                            <path
-                                d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                        </svg>
+                    <a href="https://www.linkedin.com/in/donald-kenneth-degbessou-684b74149/">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" width="24" height="24"><path xmlns="http://www.w3.org/2000/svg" d="M41.64 3H6.59C4.673 3 3 4.378 3 6.27V41.4C3 43.305 4.673 45 6.589 45H41.63c1.926 0 3.37-1.707 3.37-3.6V6.27C45.011 4.379 43.556 3 41.64 3M16.02 38.01h-6.018V19.3h6.017zm-2.801-21.553h-.043c-1.926 0-3.173-1.434-3.173-3.228 0-1.827 1.28-3.227 3.249-3.227s3.173 1.39 3.216 3.227c0 1.794-1.248 3.228-3.25 3.228M38.01 38.009h-6.017V27.78c0-2.45-.875-4.125-3.052-4.125-1.663 0-2.648 1.125-3.086 2.22-.164.395-.208.93-.208 1.478V38.01H19.63V19.301h6.017v2.604c.876-1.247 2.244-3.042 5.426-3.042 3.95 0 6.938 2.604 6.938 8.217z"></path></svg>
                     </a>
-                    <a>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            className="fill-current">
-                            <path
-                                d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                        </svg>
+                    <a href="mailto:degbessou.ca@gmail.com?subject=Proposition%20d%27offre%20d%27emploi&body=Bonjour...%0D%0A%0D%0A">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" width="24" height="24"><path xmlns="http://www.w3.org/2000/svg" d="M39.75 7.5H8.25A5.256 5.256 0 0 0 3 12.75v22.5a5.256 5.256 0 0 0 5.25 5.25h31.5A5.256 5.256 0 0 0 45 35.25v-22.5a5.256 5.256 0 0 0-5.25-5.25m-1.33 8.684-13.5 10.5a1.5 1.5 0 0 1-1.84 0l-13.5-10.5a1.5 1.5 0 1 1 1.84-2.368L24 23.6l12.58-9.784a1.5 1.5 0 0 1 1.84 2.368"></path></svg>
                     </a>
-                    <a>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            className="fill-current">
-                            <path
-                                d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                        </svg>
+                    <a href="https://www.instagram.com/deg.bessou/">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" width="24" height="24"><path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" d="M16 4h16a12 12 0 0 1 12 12v16a12 12 0 0 1-12 12H16A12 12 0 0 1 4 32V16A12 12 0 0 1 16 4m0 4a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h16a8 8 0 0 0 8-8V16a8 8 0 0 0-8-8zm8 26a10 10 0 1 1 0-20 10 10 0 0 1 0 20m0-4a6 6 0 1 0 0-12 6 6 0 0 0 0 12m10-14a2 2 0 1 1 0-4 2 2 0 0 1 0 4"></path></svg>
                     </a>
                 </div>
             </nav>
