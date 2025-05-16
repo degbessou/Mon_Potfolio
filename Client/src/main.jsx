60a456791deb205966ff154d87ff63bb4cab3c84
degbessou <dedokema@gmail.com>
2025-05-15 21:44:56 -0400

    Structure du portfolio terminée : ajout de la faq, contact et footer

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,6 +6,10 @@ import About from './components/About'
 import Experiences from './components/Experiences'
 import Projects from './components/Projects'
 import Studies from './components/Studies'
+import Description from './components/Description'
+import Description2 from './components/Description2'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,5 +18,9 @@ createRoot(document.getElementById('root')).render(
     <Experiences />
     <Projects />
     <Studies />
+    <Description2 />
+    <Description />
+    <Contact />
+    <Footer />
   </StrictMode>,
 )
