fd1c64fe623528065b39008f22303c921769e097
degbessou <dedokema@gmail.com>
2026-01-08 22:14:46 -0500

    fix section experiences responsive

---
+++ Client/src/assets/experiencesData.jsx
@@ -0,0 +1,32 @@
+
+export const experiencesData = [
+    {
+        date: "Février 2022 – Aujourd’hui",
+        entreprise: "Secrétariat du Conseil du Trésor",
+        location: "Québec, Canada",
+        logo: "/pictures/sct_logo.png",
+        position: "Analyste / Développeur informatique",
+        level: "Sénior",
+        description: `
+      Conception de tableaux de bord Power BI percutants.
+      Développement de requêtes SQL complexes.
+      Création de sites web et applications légères.
+      Optimisation des visualisations et personnalisation UI.
+      `,
+        details: "employee"
+    },
+    {
+        date: "Octobre 2024 – Aujourd’hui",
+        entreprise: "Développeur indépendant",
+        location: "Québec, Canada",
+        logo: "/pictures/freelance.png",
+        position: "Développeur BI & Fullstack",
+        level: "Sénior",
+        description: `
+      Développement de solutions BI.
+      Création de sites vitrines et applications web.
+      Maintenance et accompagnement client.
+      `,
+        details: "freelance"
+    }
+];
